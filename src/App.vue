9dd302a17a8ead69275f6742410eb4eb5d6aeea8
stuyk <trevorwessel@protonmail.com>
2025-03-20 20:13:25 -0600

    fix: build

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, onMounted } from 'vue';
+import { onMounted } from 'vue';
 import { useForum } from './composables/useForum';
 import Navbar from './components/Navbar.vue';
 
